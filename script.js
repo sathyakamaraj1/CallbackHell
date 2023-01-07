8fa4f2eebf4b5ba1e643978c58e600fd962ff1c7
sathyakamaraj1 <sathyakamarajrks@gmail.com>
2023-01-08 00:54:28 +0530

    code update

---
+++ script.js
@@ -0,0 +1,36 @@
+let timer = 10;
+setTimeout(() => {
+    document.getElementById('timer1').innerHTML = timer--;
+    setTimeout(() => {
+        document.getElementById('timer1').innerHTML = timer--;
+        setTimeout(() => {
+            document.getElementById('timer1').innerHTML = timer--;
+            setTimeout(() => {
+                document.getElementById('timer1').innerHTML = timer--;
+                setTimeout(() => {
+                    document.getElementById('timer1').innerHTML = timer--;
+                    setTimeout(() => {
+                        document.getElementById('timer1').innerHTML = timer--;
+                        setTimeout(() => {
+                            document.getElementById('timer1').innerHTML = timer--;
+                            setTimeout(() => {
+                                document.getElementById('timer1').innerHTML = timer--;
+                                setTimeout(() => {
+                                    document.getElementById('timer1').innerHTML = timer--;
+                                    setTimeout(() => {
+                                        document.getElementById('timer1').innerHTML = timer--;
+                                        setTimeout(() => {
+                                            document.getElementById('layout').innerHTML = "Happy Independance Day!!🎉🎉🎉";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+
+    }, 1000)
+
+}, 1000);
